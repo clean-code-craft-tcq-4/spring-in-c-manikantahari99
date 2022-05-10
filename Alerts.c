c983b40bb3812d223cc6ddb5441b46572e74e03f
manikantahari99 <105211979+manikantahari99@users.noreply.github.com>
2022-05-10 14:02:27 +0530

    Update Alerts.c

--- Alerts.c
+++ Alerts.c
@@ -1,1 +1,15 @@
+#include "stdio.h"
+#include "Alerts.h"
 
+
+int emailAlertCallCount = 0;
+int ledAlertCallCount = 0;
+
+void emailAlerter(void)
+{
+	emailAlertCallCount = 1;
+}
+void ledAlerter(void)
+{
+	ledAlertCallCount = 1;
+}
