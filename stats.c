214e9a388cf45263b365f7b7709436d46d7328c6
manikantahari99 <105211979+manikantahari99@users.noreply.github.com>
2022-05-10 13:32:55 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,11 +1,65 @@
 #include "stats.h"
+#include "stdio.h"
 
-struct Stats compute_statistics(const float* numberset, int setlength) {
-    struct Stats s;
-    s.average = 0;
-    s.min = 0;
-    s.max = 0;
-}
 
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
+
+
+//This Function is used to calculate the Min, Max and Average.
+struct Stats compute_statistics(const float* numberset, int setlength)
+{
+	float average;
+	float sum = 0;
+	float min = 0;
+	float max = 0;
+	struct Stats s;
+	int count = 0;
+
+	if (numberset != '\0')
+	{
+		min = numberset[0];
+		max = numberset[0];
+
+		while (count < setlength)
+		{
+			if (numberset[count] > max)
+				max = numberset[count];
+			if (numberset[count] < min)
+				min = numberset[count];
+
+			sum = sum + numberset[count];
+
+			count++;
+		}
+	}
+	s.average = 1.0*sum / setlength;
+	s.max = max;
+	s.min = min;
+	return s;
+}
+
+//This Function is used to Check the threshold and Alert the user.
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
+{
+	if (alerters != '\0')
+	{
+		/*If the max value is greater than the Threshold value. It should call email Alert and Led Alert*/
+		if (computedStats.max > maxThreshold)
+		{
+			alerters[0]();
+			alerters[1]();
+		}
+	}
+
+}
+
+
+void emailAlerter(void)
+{
+	emailAlertCallCount = 1;
+}
+void ledAlerter(void)
+{
+	ledAlertCallCount = 1;
+}
