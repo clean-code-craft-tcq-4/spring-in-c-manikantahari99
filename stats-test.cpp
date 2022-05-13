9b41da050404a4de37a8a7bbc2ed090eaece29ec
manikantahari99 <105211979+manikantahari99@users.noreply.github.com>
2022-05-13 12:21:58 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -13,8 +13,8 @@ TEST_CASE("reports average, minimum and maximum") {
     struct Stats computedStats = compute_statistics(numberset, setlength);
     float epsilon = 0.001f;
     REQUIRE(abs(computedStats.average - 4.525) < epsilon);
-    REQUIRE(abs(computedStats.max - 8.9) < epsilon);
     REQUIRE(abs(computedStats.min - 1.5) < epsilon);
+    REQUIRE(abs(computedStats.max - 8.9) < epsilon);
 }
 
 TEST_CASE("average is NaN for empty array") {
